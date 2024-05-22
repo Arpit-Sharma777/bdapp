e7ac23a42c86d4a352520cd9d33b56a92671e935
Arpit Sharma <arpit.31093@gmail.com>
2024-05-22 23:58:09 +0530

    views and form updated

--- blood_donor/donors/views.py
+++ blood_donor/donors/views.py
@@ -17,14 +17,24 @@ class SearchForm(forms.Form):
     blood_group = forms.ChoiceField(choices=BLOOD_GROUPS)
 
 def register_donor(request):
-    if request.method == 'POST':
-        form = DonorForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('donor_detail')  # or any other success URL
-    else:
-        form = DonorForm()
-    return render(request, 'register.html', {'form': form})
+    if request.method == "POST":
+        name = request.POST.get('name')
+        age = request.POST.get('age')
+        weight = request.POST.get('weight')
+        email = request.POST.get('email')
+        blood_group = request.POST.get('blood_group')
+        last_donation_date = request.POST.get('last_donation_date')
+        phone_number = request.POST.get('phone_number')
+        address = request.POST.get('address')
+        city = request.POST.get('city')
+        state = request.POST.get('state')
+        zip_code = request.POST.get('zip_code')
+        Donor.object.create(name=name, age=age, weight=weight, email=email, blood_group=blood_group, 
+                      last_donation_date=last_donation_date, phone_number=phone_number, 
+                      address=address, city=city, state=state, zip_code=zip_code)
+        donor.save()
+        return redirect('donor_detail')  # or any other success URL
+    return render(request, 'register.html')
 
 def search(request):
     if request.method == "GET":
